509d2d13fff7ca9dd6febbf4182645d856a646a4
Al <alpalma95@gmail.com>
2024-07-23 18:07:29 +0200

    Add routes and active page based on data

--- src/data.js
+++
@@ -1,5 +0,0 @@
-export const colors = {
-    yellow: '#fde758',
-    red: '#EF9CA6',
-    purple: '#CE98FF',
-}
---
+++ src/data.ts
@@ -0,0 +1,28 @@
+export const colors = {
+    yellow: '#fde758',
+    red: '#EF9CA6',
+    purple: '#CE98FF',
+}
+
+export const routes: { name: string, path: string }[] = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Speakers',
+        path: '/speakers',
+    },
+    {
+        name: 'Submission',
+        path: '/submission',
+    },
+    {
+        name: 'Travel & Accommodation',
+        path: '/travel-and-accommodation',
+    },
+    {
+        name: 'About the Conference',
+        path: '/about-the-conference',
+    }
+]
